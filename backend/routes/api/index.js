4aa6072ce9c4bfbb33b8746584cda693e5340144
Lyndon <tzlyndon@gmail.com>
2023-11-22 17:31:33 -0800

    Added User login endpoints

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,13 +1,18 @@
 const router = require('express').Router();
-// GET /api/restore-user
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { restoreUser } = require('../../utils/auth.js');
 router.use(restoreUser);
 
-// router.post('/test', function(req, res) {
-//   res.json({ requestBody: req.body });
-// });
+router.use('/session', sessionRouter);
 
-router.use(restoreUser);
+router.use('/users', usersRouter);
+
+router.post('/test', function(req, res) {
+  res.json({ requestBody: req.body });
+});
+
+
 // GET /api/set-token-cookie
 // const { setTokenCookie } = require('../../utils/auth.js');
 // const { User } = require('../../db/models');
